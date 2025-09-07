cd3e7478f99496cf9b75b8ae984df31b6036cba2
ammaproficient <ammarproficient@gmail.com>
2025-09-07 12:05:02 +0500

    friendship model added

--- connections/admin.py
+++ connections/admin.py
@@ -1,20 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Message
-
-
-# class CustomUserAdmin(UserAdmin):
-#     model = CustomUser
-#     list_display = ["username", "email", "is_staff", "is_active"]
-#
-# admin.site.register(CustomUser, CustomUserAdmin)
-#
-# # admin.site.register(Message)
-#
-# @admin.register(Message)
-# class MessageAdmin(admin.ModelAdmin):
-#     list_display = ("user", "room", "content", "timestamp", "message_deliver", "message_read")
-#     search_fields = ("room", "content")
+from .models import CustomUser, Message, Friendship
 
 
 @admin.register(Message)
@@ -28,3 +13,4 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_display = ("id", "username", "email", "bio", "is_active", "is_staff")
     search_fields = ("username", "email")
 
+admin.site.register(Friendship)
--- connections/models.py
+++ connections/models.py
@@ -25,3 +25,17 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.user.username}: {self.content[:20]}"
+
+
+class Friendship(models.Model):
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="friendship_requests_sent", on_delete=models.CASCADE)
+    to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="friendship_requests_received", on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=[("pending", "Pending"), ("accepted", "Accepted"), ("rejected", "Rejected"),], default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("from_user", "to_user")  # aik hi banda ko bar bar request na bheji jaay
+
+    def __str__(self):
+        return f"{self.from_user.username} -> {self.to_user.username} ({self.status})"
+
